90651dcd4ac008c96571d1f3d2986eda27f6f29d
Денис Петровский <petrovsky.dv@gmail.com>
2021-01-18 08:20:23 +0300

    refactor

--- main.py
+++ main.py
@@ -7,29 +7,13 @@ from pprint import pprint
 from dotenv import load_dotenv
 import telegram
 
-load_dotenv()
-telegram_token = os.getenv('TELEGRAM_TOKEN')
-telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
-devman_token = os.getenv('DEVMAN_TOKEN')
-bot = telegram.Bot(token=telegram_token)
-
-# add filemode="w" to overwrite
 logging.basicConfig(level=logging.DEBUG)
 
-
-# headers = {
-#     'Authorization': f'Token {telegram_token}'
-# }
-#
-# params = {
-#     'timestamp': ''
-# }
-
-# def send_telergam_message(text):
-#     bot = telegram.Bot(token=telegram_token)
-#     bot.send_message(chat_id=telegram_chat_id,
-#                      text=text,
-#                      parse_mode=telegram.ParseMode.HTML)
+load_dotenv()
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+DEVMAN_TOKEN = os.getenv('DEVMAN_TOKEN')
+BOT = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def get_checks(token):
@@ -46,34 +30,27 @@ def long_pooling_check(token):
     headers = {
         'Authorization': f'Token {token}'
     }
-
     params = {
         'timestamp': ''
     }
+
     while True:
         try:
             response = requests.get('https://dvmn.org/api/long_polling/', headers=headers, params=params)
             response.raise_for_status()
-            answ = response.json()
-            pprint(answ)
-            if answ['status'] == 'timeout':
-                print(answ['timestamp_to_request'])
-                params['timestamp'] = answ['timestamp_to_request']
-                pprint(params)
-            elif answ['status'] == 'found':
-                lesson_title = answ['new_attempts'][0]['lesson_title']
-                lesson_url = answ['new_attempts'][0]['lesson_url']
-                if answ['new_attempts'][0]['is_negative']:
+            response_result = response.json()
+            if response_result['status'] == 'timeout':
+                params['timestamp'] = response_result['timestamp_to_request']
+            elif response_result['status'] == 'found':
+                lesson_title = response_result['new_attempts'][0]['lesson_title']
+                lesson_url = response_result['new_attempts'][0]['lesson_url']
+                if response_result['new_attempts'][0]['is_negative']:
                     next_step = 'Необходимо доработать решение.'
                 else:
                     next_step = 'Решение принято. Можно приступать к следующему уроку!'
                 message = f'Проверена работа "{lesson_title}"\nhttps://dvmn.org{lesson_url}\n{next_step}'
-                print(message)
-                # send_telergam_message(message)
-                bot.send_message(chat_id=telegram_chat_id,
-                                 text=message)
-                print(answ['last_attempt_timestamp'])
-                params['timestamp'] = answ['last_attempt_timestamp']
+                BOT.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+                params['timestamp'] = response_result['last_attempt_timestamp']
         except requests.exceptions.ReadTimeout:
             print("\n Переподключение к серверу \n")
             time.sleep(5)
@@ -83,13 +60,4 @@ def long_pooling_check(token):
 
 
 if __name__ == '__main__':
-    # main()
-    # response = requests.get('https://dvmn.org/api/long_polling/', headers=headers, params=params)
-    # response.raise_for_status()
-    # answ = response.json()
-    # pprint(answ)
-
-    # get_checks(devman_token)
-
-    long_pooling_check(devman_token)
-    # send_telergam_message()
+    long_pooling_check(DEVMAN_TOKEN)
